3031af58a73cafc943feee080830b3cde66f08fb
John Flaherty <slowcircuits@gmail.com>
2022-07-28 10:46:23 -0700

    Removed minimap rotation

--- ImprovedMinimap.cs
+++ ImprovedMinimap.cs
@@ -20,30 +20,24 @@ namespace ImprovedMinimap {
         public static ManualLogSource StaticLogger;
         public const string pluginGuid = "tinyresort.dinkum.improvedminimap";
         public const string pluginName = "Improved Minimap";
-        public const string pluginVersion = "0.2.0";
+        public const string pluginVersion = "0.2.5";
         public static ConfigEntry<bool> debugMode;
-        public static ConfigEntry<KeyCode> lockHotkey;
         public static ConfigEntry<KeyCode> zoomInHotkey;
         public static ConfigEntry<KeyCode> zoomOutHotkey;
         public static ConfigEntry<float> defaultZoom;
-        public static ConfigEntry<bool> lockMinimapRotation;
         public static ConfigEntry<KeyCode> hideMinimapHotkey;
         public static bool forceClearNotification;
         public static float Zoom = 1;
-        public static bool minimapRotationLocked;
         public static bool showMinimap = true;
 
         private void Awake() {
 
-            lockMinimapRotation = Config.Bind<bool>("General", "LockMinimapRotation", true, "If true, the minimap will always point north.");
             defaultZoom = Config.Bind<float>("General", "DefaultZoom", 1f, "Default zoom level of minimap. Value of 1 keeps normal game zoom. Lower values are more zoomed out. Higher values are more zoomed in. Range: 0.20 - 1.25");
             debugMode = Config.Bind<bool>("General", "DebugMode", false, "If true, the BepinEx console will print out debug messages related to this mod.");
-            lockHotkey = Config.Bind<KeyCode>("Keybinds", "MinimapLockHotkey", KeyCode.None, "Keybind for toggling the minimap rotation lock. (Set to None to disable)");
             hideMinimapHotkey = Config.Bind<KeyCode>("Keybinds", "HideMinimapHotkey", KeyCode.None, "Keybind for toggling whether or not the minimap is shown. (Set to None to disable)");
             zoomInHotkey = Config.Bind<KeyCode>("Keybinds", "ZoomInHotkey", KeyCode.None, "Keybind for zooming in on the minimap. (Set to None to disable)");
             zoomOutHotkey = Config.Bind<KeyCode>("Keybinds", "ZoomOutHotkey", KeyCode.None, "Keybind for zooming out on the minimap. (Set to None to disable)");
             Zoom = defaultZoom.Value;
-            minimapRotationLocked = lockMinimapRotation.Value;
 
             #region Logging
             StaticLogger = Logger;
@@ -65,10 +59,6 @@ namespace ImprovedMinimap {
         }
 
         public void Update() {
-            if (Input.GetKeyDown(lockHotkey.Value)) {
-                minimapRotationLocked = !minimapRotationLocked;
-                notify("Improved Minimap", "Rotation Lock: " + (minimapRotationLocked ? "ENABLED" : "DISABLED"));
-            }
             if (Input.GetKeyDown(zoomInHotkey.Value)) { Zoom += 0.05f; }
             if (Input.GetKeyDown(zoomOutHotkey.Value)) { Zoom -= 0.05f; }
             if (Input.GetKeyDown(hideMinimapHotkey.Value)) { showMinimap = !showMinimap; }
@@ -86,11 +76,7 @@ namespace ImprovedMinimap {
                 !CheatScript.cheat.cheatMenuOpen && __instance.mapCircle.gameObject.activeSelf != showMinimap)
                 __instance.mapCircle.gameObject.SetActive(showMinimap);
 
-            if (!__instance.mapOpen) {
-                RenderMap.map.scale = Zoom * 20f;
-                RenderMap.map.lockMapNorth = minimapRotationLocked;
-                if (minimapRotationLocked) { __instance.charPointer.localRotation = Quaternion.Euler(0f, 0f, 0f); }
-            }
+            if (!__instance.mapOpen) { RenderMap.map.scale = Zoom * 20f; }
         }
 
         public static void notify(string title, string subtitle) {
